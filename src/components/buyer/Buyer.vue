940ee5eb576ae4661426ea55715eb7b55bbf63b5
unknown <ivangelina@abv.bg>
2019-03-19 19:42:10 +0200

    Home change

--- src/components/buyer/Buyer.vue
+++ src/components/buyer/Buyer.vue
@@ -115,22 +115,7 @@ export default {
       }
      
     },
-    methods: {
-       chas(){
-       var vreme = new Chas();
-       var hh = vreme.getHours();
-       var mm = vreme.getMinutes();
-       var ss = vreme.getSeconds(); 
-       
-       if (hh <= 9) hh = "0" + hh;
-       if (mm <= 9) mm = "0" + mm;
-       if (ss <= 9) ss = "0" + ss;
-
-       document.forms.ch.chasovnik.value = hh + ": " +mm+ ": " +ss;
-
-       window.setTimeout ("chas()", 1000);
-      }
-    },
+   
 
    methods: {
        chas(){
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,14 +1,45 @@
 <template>
 <b-container class="bv-example-row bv-example-row-flex-cols">
-  <b-img class="mt-4" center src="https://picsum.photos/1100/350/?image=84" alt="Center image" />
-  <div>
+   <br>
+       <h4>ПРИЛОЖЕНИЕ В ПОМОЩ НА БЪЛГАРСКИЯ ПРЕДПРИЕМАЧ И БЪЛГАРСКИЯ КУПУВАЧ</h4>
+   <div class="row">
+     <br>
+     <div role="group" class="col-md-2"> 
+          </div> 
+<div role="group" class="col-md-8">
+<br> 
+<div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 30px #000"
+    fade
+    indicators
+    img-width="1100"
+    img-height="1000"
+  >
+  <br>
+    <b-carousel-slide caption="Жилищна сграда" img-src="/static/images/residental.jpg" alt="Center image"/>
+    <b-carousel-slide caption="Офис сграда" img-src="/static/images/office.jpg" alt="Center image" />
+    <b-carousel-slide caption="Промишлена сграда" img-src="/static/images/industrial.JPG" alt="Center image"/>
+    <b-carousel-slide caption="Търговска сграда" img-src="/static/images/shopping.jpg" alt="Center image"/>
+  </b-carousel>
+</div>
+</div>
+ <div role="group" class="col-md-2"> 
+          </div> 
+</div>
+<br>
+
+
   <b-card no-body>
     <b-tabs card>
       <b-tab title="Секция Предприемач" active>Изчисляване на всички разходи за построяването на строителен обект</b-tab>
       <b-tab title="Секция Купувач">Тази секция ще бъде разработена на по-късен етап</b-tab>
-    </b-tabs>
+    </b-tabs>     
   </b-card>
   <div>
+    <br>
+    <br>
   <b-embed
     type="iframe"
     aspect="16by9"
